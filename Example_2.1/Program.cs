33e2edc7a0f655d62e5e2cd29af7856c5323d3f0
Anastasya Komleva <008kitti@mail.ru>
2022-12-20 20:27:12 +0300

    new names

---
+++ Example_2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 2;
+int b = 10;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+else 
+{
+    
---
+++ Example_2/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 5;
+int b = 7;
+
+int max = a;
+
+if (a > max) max = a;
+{
+    Console.Write("max = a");
+}
+
+else
+{
+    
+} (b < max) min = b;
+{
+    Console.Write("min = b");
+}
+
+Console.WriteLine(max);
+
+Console.WriteLine(min);
