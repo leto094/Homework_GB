387f9c8e2212e1c2f20767ba665488c3f4d0ab32
Anastasya Komleva <008kitti@mail.ru>
2022-12-20 23:01:08 +0300

    create new exercise

---
+++ Example_6/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число n");
+int n = Int32.Parse(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число нечетное");
+}
