e81f10c5239c08ffc2174425cfde50fecfdcd459
Anastasya Komleva <008kitti@mail.ru>
2022-12-20 23:52:28 +0300

    add new epample

---
+++ Example_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число n");
+int n = Int32.Parse(Console.ReadLine());
+
+int count = 1;
+
+while(count < n+1)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count = count + 1;
+
+}
