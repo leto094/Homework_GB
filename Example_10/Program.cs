5ef5ce612a13a0b6699c340b0820adc9c9fa2e47
Anastasya Komleva <008kitti@mail.ru>
2022-12-22 13:16:58 +0300

    exercise 10 from homework

---
+++ Example_10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число от 100 до 999");
+int n = Int32.Parse(Console.ReadLine());
+
+int GetNumber(int value)
+{
+    int a = value / 100;
+    int b = (value / 10) % 10;
+    int c = value % 10;
+
+    return a * 10 + c;
+}
+
+System.Console.WriteLine(GetNumber(n));
