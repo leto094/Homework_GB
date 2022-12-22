72175c36cf5c77ce54ce2e16e5cd353abffdf14b
Anastasya Komleva <008kitti@mail.ru>
2022-12-22 18:50:05 +0300

    creat new text

--- Example_10/Program.cs
+++ Example_10/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число от 100 до 999");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число от 100 до 999");
 int n = Int32.Parse(Console.ReadLine());
 
 int GetNumber(int value)
@@ -7,7 +9,7 @@ int GetNumber(int value)
     int b = (value / 10) % 10;
     int c = value % 10;
 
-    return a * 10 + c;
+    return b;
 }
 
 System.Console.WriteLine(GetNumber(n));
