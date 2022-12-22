43aec144e3eba6cdfb3915220aca7820f8f76a85
Anastasya Komleva <008kitti@mail.ru>
2022-12-22 22:10:31 +0300

    exercise 13

---
+++ Example_13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число от 100 до 999");
+int n = Int32.Parse(Console.ReadLine());
+
+if ((99 < n) && (n < 1000))
+{
+    Console.WriteLine(n % 10);
+    Console.WriteLine("Это успех!");
+}   else {
+    Console.WriteLine("Error");
+}
+    
+
