59476f92bb6a253830ebedb3a05f29ff523467af
Anastasya Komleva <008kitti@mail.ru>
2023-01-02 21:59:29 +0300

    29

---
+++ Example_29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+void FillArray(int [] numbers)
+{
+    int length = numbers.Length;
+    int index = 0;
+    while (index < length)
+    {
+        numbers[index] = new Random().Next(1, 8);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] dates)
+{
+    int count = dates.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(dates[position]);
+        Console.Write(" ");
+        position++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
