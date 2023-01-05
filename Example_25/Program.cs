72e5e6f6b5f6cd0f81c95641282e4935d1cebcea
Anastasya Komleva <008kitti@mail.ru>
2023-01-05 21:39:16 +0300

    задача 25

---
+++ Example_25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл (программу?), который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.WriteLine("введите число A");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите число B");
+int B = int.Parse(Console.ReadLine());
+
+double result = Math.Pow(A,B);
+
+Console.WriteLine($"число А = {A} в степени B = {B} равно {result}");
